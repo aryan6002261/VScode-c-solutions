c26e36150c69ac61de1f2e662c16cec280c45373
Aryan Sharma <aryan6002261@gmail.com>
2026-08-16 21:37:42 +0530

    Adding stdlib header file and dynamic memory allocation for temporary arrays

--- Sorting/merge-sort.c
+++ Sorting/merge-sort.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define MAX 100
 
 void read(int nums[MAX], int n) {
@@ -23,15 +24,15 @@ void merge(int nums[], int left, int mid, int right) {
 	int *L = (int *)malloc(n1 * sizeof(int));
     int *R = (int *)malloc(n2 * sizeof(int));
 
-    for (i=0; i<n1; i++) L[i] = arr[left + i];
-    for (j=0; j<n2; j++) R[j] = arr[mid + 1 + j];
+    for (i=0; i<n1; i++) L[i] = nums[left + i];
+    for (j=0; j<n2; j++) R[j] = nums[mid + 1 + j];
 
     i = 0; j = 0; k = left;
     while (i<n1 && j<n2) {
-        arr[k++] = (L[i]<=R[j]) ? L[i++] : R[j++];
+        nums[k++] = (L[i]<=R[j]) ? L[i++] : R[j++];
     }
-    while (i < n1) arr[k++] = L[i++];
-    while (j < n2) arr[k++] = R[j++];
+    while (i < n1) nums[k++] = L[i++];
+    while (j < n2) nums[k++] = R[j++];
     
     free(L);
     free(R);
@@ -41,9 +42,9 @@ void merge(int nums[], int left, int mid, int right) {
 void mergeSort(int nums[], int left, int right) {
     if (left < right) {
         int mid = (left+right)/2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+        merge(nums, left, mid, right);
     }
 }
 
